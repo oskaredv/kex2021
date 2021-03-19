db1a88ed11a03ac95981babf6fac690f90a4cc69
mnordste <mnordste@kth.se>
2021-03-19 14:57:04 +0100

    updated urls

--- kex2021/urls.py
+++ kex2021/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('questions/', include('questions.urls')),
+    path('', include('questions.urls')),
     path('admin/', admin.site.urls),
 ]
