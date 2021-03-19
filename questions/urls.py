dabbbe78acb995912d9476f222ce802bb6e84ebb
mnordste <mnordste@kth.se>
2021-03-19 15:05:51 +0100

    forgot comma

--- questions/urls.py
+++ questions/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('questions/', views.frontpage, name='frontpage')
+    path('questions/', views.frontpage, name='frontpage'),
     path('', views.landing, name='landing')
 ]
