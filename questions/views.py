cbdb241cfae2ba51d5a96a977898daa2aae0a760
mnordste <mnordste@kth.se>
2021-03-19 14:33:50 +0100

    imported httprequest in views.py

--- questions/views.py
+++ questions/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
 
 def firstpagetest(request):
     return HttpResponse('Saker funkar')
