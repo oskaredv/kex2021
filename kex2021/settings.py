9ec7fb715e1363ad2f1de2100ea531bd33d2456b
mnordste <mnordste@kth.se>
2021-03-22 11:37:18 +0100

    Updated database settings for posgreSQL

--- kex2021/settings.py
+++ kex2021/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'kex2021.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgreSQL',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
