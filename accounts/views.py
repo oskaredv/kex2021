01bcb2e56c190da3701cd21d66a503228a2920ba
mnordste <mnordste@kth.se>
2021-03-22 16:10:05 +0100

    custom login and signup views

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.template import loader
+from accounts.forms import UserForm
+
+from django.contrib.auth import authenticate, login
+
+def login(request):
+    template = loader.get_template('accounts/login.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect('questions/')
+
+    else:
+        # Return an 'invalid login' error message.
+        return HttpResponse('felaktiga uppgifter')
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
--- questions/views.py
+++ questions/views.py
@@ -3,19 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-from django.contrib.auth import authenticate, login
 
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect('questions/')
-
-    else:
-        # Return an 'invalid login' error message.
-        return HttpResponse('felaktiga uppgifter')
 
 def landing(request):
     return HttpResponse('välkommen')
