705c1df75a60f1ededdc7246e954deaf0307a215
mnordste <mnordste@kth.se>
2021-03-22 16:27:23 +0100

    added import for httpResponse in correct place

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.template import loader
 from accounts.forms import UserForm
+from django.http import HttpResponse
 
 from django.contrib.auth import authenticate, login
 
