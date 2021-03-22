e05065f12b4c8eed5ce53c2edfa4973748d7283e
mnordste <mnordste@kth.se>
2021-03-22 14:53:22 +0100

    logged in homepage

--- kex2021/urls.py
+++ kex2021/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', include('questions.urls')),
+    #path('', include('questions.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
