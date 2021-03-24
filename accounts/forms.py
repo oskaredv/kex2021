9f059349c7d9c6f500dea086080ee4ade5b5adce
mnordste <mnordste@kth.se>
2021-03-24 13:29:52 +0100

    removved first and last name from user creation

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserForm(UserCreationForm):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ('first_name','last_name', 'username', 'email', 'password1' ,'password2' )
+        fields = ('username', 'email', 'password1' ,'password2' )
