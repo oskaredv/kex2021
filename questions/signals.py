c7df18fc8af2c84a965b1add0fd1a48681450632
mnordste <mnordste@kth.se>
2021-04-07 14:08:59 +0200

    added datetime import

--- questions/signals.py
+++ questions/signals.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Question, Profile
+from datetime import datetime
 
 
 @receiver(post_save, sender=User)
