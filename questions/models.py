1d1ed89052276f40c37d43b5da9e71ba602a4f9c
mnordste <mnordste@kth.se>
2021-04-07 12:04:52 +0200

    added test model

--- questions/models.py
+++ questions/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 
+class Test(models.Model):
+    test = models.IntegerField(default=0)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
