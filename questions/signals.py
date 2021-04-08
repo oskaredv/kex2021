fb5f36f7165cc273a399c29b5f2fd2648af30b60
mnordste <mnordste@kth.se>
2021-04-08 11:24:58 +0200

    changed signals to only fire if question is created, not edited

--- questions/signals.py
+++ questions/signals.py
@@ -19,23 +19,24 @@ def save_user_profile(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Question)
 def update_streak(sender, instance, **kwargs):
-    profile = Profile.objects.get(user = instance.user)
-    questions = Question.objects.filter(user=instance.user).order_by('-pub_date')
-    if questions.exists():
-        delta = (datetime.date.today - questions.first().pub_date).days
-        #delta = (datetime.now().date() - questions.first().pub_date.date()).days
-        if delta == 1:
-            profile.increment_streak()
-        elif delta > 1:
+    if kwargs.get('created', True):
+        profile = Profile.objects.get(user = instance.user)
+        questions = Question.objects.filter(user=instance.user).order_by('-pub_date')
+        if questions.exists():
+            delta = (datetime.date.today - questions.first().pub_date).days
+            #delta = (datetime.now().date() - questions.first().pub_date.date()).days
+            if delta == 1:
+                profile.increment_streak()
+            elif delta > 1:
+                profile.streak = 1
+        else: 
             profile.streak = 1
-    else: 
-        profile.streak = 1
-    
     profile.save()
 
 @receiver(post_save, sender=Question)
 def update_profile(sender, instance, **kwargs):
-    profile = Profile.objects.get(user = instance.user)   
-    profile.increment_num_questions()
+    if kwargs.get('created', True):
+        profile = Profile.objects.get(user = instance.user)   
+        profile.increment_num_questions()
     profile.save()
        
