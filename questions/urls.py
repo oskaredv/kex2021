5c5abd54a18581c34a1ff961b22e82d48b3f5c0f
mnordste <mnordste@kth.se>
2021-04-08 10:28:30 +0200

    removed unused views

--- questions/urls.py
+++ questions/urls.py
@@ -3,7 +3,6 @@ from .views import QuestionCreateView, ProfileView, LeaderboardView
 from . import views
 
 urlpatterns = [
-    path('questions/', views.frontpage, name='frontpage'),
     path('createquestion/', QuestionCreateView.as_view(), name='createquestion'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('leaderboard/', LeaderboardView.as_view(), name='leaderboard'),
