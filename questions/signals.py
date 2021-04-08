d9e45eb74ca577c5b8ce06dff68e047dd4d1414f
mnordste <mnordste@kth.se>
2021-04-08 11:07:05 +0200

    added import for presave

--- questions/signals.py
+++ questions/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Question, Profile
 import datetime 
