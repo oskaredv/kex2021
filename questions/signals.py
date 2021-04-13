d405738d502d244bef5e58e5beb2c57e68d0b64b
mnordste <mnordste@kth.se>
2021-04-13 10:23:22 +0200

    removed ss from import in signals

--- questions/signals.py
+++ questions/signals.py
@@ -1,4 +1,4 @@
-ssfrom django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Question, Profile
