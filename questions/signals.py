1e9b730409088b49215b6fe9363ebcedb1f8caae
mnordste <mnordste@kth.se>
2021-04-13 09:36:14 +0200

    mysterious q has been removed

--- questions/signals.py
+++ questions/signals.py
@@ -23,7 +23,7 @@ def update_streak(sender, instance, **kwargs):
     if kwargs.get('created', True):
         questions = Question.objects.filter(user=instance.user).order_by('-pub_date')
         if questions.exists():
-            delta = (datetime.date.today() - questqions.first().pub_date).days
+            delta = (datetime.date.today() - questions.first().pub_date).days
             #delta = (datetime.now().date() - questions.first().pub_date.date()).days
             if delta == 1:
                 profile.increment_streak()
