b073b5df8e305ccf38cdb85e71801488a339e620
mnordste <mnordste@kth.se>
2021-04-15 09:37:19 +0200

    testing messages even more

--- questions/views.py
+++ questions/views.py
@@ -44,6 +44,7 @@ class QuestionCreateView(LoginRequiredMixin, SuccessMessageMixin, DedupMessageMi
         if profile.group:
             messages.success(request, "Your question was successfully added!" + "\n" + "You gained " + str(gained_points) + " points")
             for index in range (6):
+                messages.success(request, "testing, index: " + str(index))
                 if badges_after[index] != badges_before[index]:
                     messages.success(request, "You recieved a badge! Check it out in your profile")
         else:
