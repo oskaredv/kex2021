7b66cbea51118714c392344d29feface59828fd9
mnordste <mnordste@kth.se>
2021-04-15 11:43:02 +0200

    testing to format form in forms.py

--- questions/forms.py
+++ questions/forms.py
@@ -1,17 +1,33 @@
 from django.forms import ModelForm
 from .models import Question
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Row, Div, Layout
 
 class CreateQuestionForm(ModelForm):
-
-    '''def save(self, *args, **kwargs):
-        if self.CreateQuestionForm.is_valid():
-            #obj = super(ModelForm, self).save(*args, **kwargs)
-            question = super().save(user=self.request.user, COMMIT=False)
-            #if self.request:
-            #    obj.user = self.request.user
-                #user = User.objects.get(username=request.user.username)
-            question.save()
-            return redirect('createquestion')'''
+    question_text = forms.CharField(label='questiontext', help_text='This is your question', widget = forms.Textarea)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(Field(question_text), wrapper_class= 'col-md-6', css_class = 'row-fluid'),
+            ),
+            Row(
+                Field('choice_a', wrapper_class= 'col-md-6', css_class = 'row-fluid'),
+                Field('choice_a_correct', wrapper_class= 'col-md-6', css_class = 'row-fluid')
+            )
+            Row(
+                Field('choice_b', wrapper_class= 'col-md-6', css_class = 'row-fluid'),
+                Field('choice_b_correct', wrapper_class= 'col-md-6', css_class = 'row-fluid')
+            )
+            Row(
+                Field('choice_c', wrapper_class= 'col-md-6', css_class = 'row-fluid'),
+                Field('choice_c_correct', wrapper_class= 'col-md-6', css_class = 'row-fluid')
+            )
+            Row(
+                Field('choice_d', wrapper_class= 'col-md-6', css_class = 'row-fluid'),
+                Field('choice_d_correct', wrapper_class= 'col-md-6', css_class = 'row-fluid')
+            )
+        )
 
     class Meta:
         model = Question
