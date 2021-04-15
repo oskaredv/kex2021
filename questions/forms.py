66a8dcf828abef14519db4ae38694c82438ad82b
mnordste <mnordste@kth.se>
2021-04-15 11:51:31 +0200

    added import for forms

--- questions/forms.py
+++ questions/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Question
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Row, Div, Layout
